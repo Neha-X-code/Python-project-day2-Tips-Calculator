ee9d022c158fabe2dd032d0fd938d3eb3d78dc59
Neha Arun <nehaarun18@gmail.com>
2023-09-27 00:42:46 +0530

    Update code

--- Calulator.py
+++ Calulator.py
@@ -3,5 +3,6 @@ bill = float(input("Enter your total bill $"))
 percent = int(input("What percentage tip would you like to give 10, 12 or 15?"))
 people = int(input("How many people to split the bill"))
 tip = (bill+(bill*(percent/100)))/people
-tip1 = (round(tip,2))
+# tip1 = (round(tip,2))
+tip1 = "{:.2f}".format(tip)
 print(f"Each person should pay: ${tip1}")
